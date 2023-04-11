0f6ffedea0cfa007cfe7b8cd70faa10956867356
viivi-uhari <viivi.uhari@gmail.com>
2023-04-11 12:12:13 +0300

    Fix code to support the non-existing video

--- client/src/components/Video.js
+++ client/src/components/Video.js
@@ -1,10 +1,11 @@
-import video from '../videos/hostile_architecture.mp4';
+// import video from '../videos/hostile_architecture.mp4';
 import bench from '../images/bench.gif';
 
 const Video = () => {
   return (
     <video width="600" height="340" controls poster={bench}>
-      <source src={video} type="video/mp4" />
+      {/* Uncomment the import above and add src={video} once the video is uploaded to GitHub */}
+      <source type="video/mp4" />
         Your browser does not support the video tag.
     </video>
   );
