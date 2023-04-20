d9ebbeb6bb3faa720f6ee4f60ac74d9f65f5c4ba
viivi-uhari <viivi.uhari@gmail.com>
2023-04-20 15:26:22 +0300

    Add videos

--- client/src/components/Video.js
+++ client/src/components/Video.js
@@ -1,10 +1,9 @@
-// import video from '../videos/hostile_architecture.mp4';
+import video from '../videos/VJP-video-green.mp4';
 import bench from '../images/bench.gif';
 
 const Video = () => {
   return (
-    <video width="600" height="340" controls poster={bench}>
-      {/* Uncomment the import above and add src={video} once the video is uploaded to GitHub */}
+    <video width="600" height="340" src={video} controls poster={bench}>
       <source type="video/mp4" />
         Your browser does not support the video tag.
     </video>
