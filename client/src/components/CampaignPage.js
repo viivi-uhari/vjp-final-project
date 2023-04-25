8a2bb1557b9e6271ef79170bc0c6329db9e70e39
viivi-uhari <viivi.uhari@gmail.com>
2023-04-25 16:45:14 +0300

    Create header

--- client/src/components/CampaignPage.js
+++ client/src/components/CampaignPage.js
@@ -1,8 +1,27 @@
 import '../styles/App.css';
+import logo from '../images/logo.svg';
 
 const CampaignPage = () => {
   return (
-    <div></div>
+    <div style={{ 
+        display: 'flex', 
+        flexDirection: 'column', 
+        alignItems: 'center', 
+        position: 'relative',
+        marginTop: '170px', 
+      }}>
+      <div style={{ 
+        backgroundColor: '#C1EDBB', 
+        width: '100%', 
+        height: '170px',
+        position: 'fixed',
+      }}/>
+      <img src={logo} alt='The logo of the campaign that depicts a heart bench' style={{
+        position: 'fixed',
+        top: 100
+      }}/>
+      <h1 style={{ position: 'fixed', top: 180 }}>Get Involved</h1>
+    </div>
   );
 }
 
