b25ae5d4177740031ff1e4874c7ce852c54494ba
viivi-uhari <viivi.uhari@gmail.com>
2023-05-03 15:32:42 +0300

    Add logo to front page

--- client/src/components/FrontPage.js
+++ client/src/components/FrontPage.js
@@ -1,9 +1,11 @@
 import '../styles/App.css';
 import Video from './Video';
+import logo from '../images/logo.svg';
 
 const FrontPage = () => {
   return (
     <div className="App">
+      <img src={logo} alt='The logo of the campaign that depicts a heart bench' />
       <h1>Inclusive Helsinki</h1>
       <Video/>
     </div>
