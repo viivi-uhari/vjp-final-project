de61359078d3c389aa9f218654536a7160553d56
viivi-uhari <viivi.uhari@gmail.com>
2023-05-07 20:53:20 +0300

    Delete unused tests

--- client/src/tests/App.test.js
+++
@@ -1,8 +0,0 @@
-import { render, screen } from '@testing-library/react';
-import App from '../App';
-
-test('Renders the title of the website', () => {
-  render(<App />);
-  const title = screen.getByText(/Inclusive Helsinki/i);
-  expect(title).toBeInTheDocument();
-});
